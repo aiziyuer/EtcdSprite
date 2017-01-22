8da8d6f16d77aa0b2a649221c0b320531be0eee5
susu <360858329@qq.com>
2017-01-22 23:14:53 +0800

    提交一个样例代码

--- src/main/java/com/aiziyuer/app/CoreApplication.java
+++ src/main/java/com/aiziyuer/app/CoreApplication.java
@@ -1,8 +1,18 @@
 package com.aiziyuer.app;
 
+import org.eclipse.swt.widgets.Display;
+
 public class CoreApplication {
 
 	public static void main(String[] args) {
-		System.out.println("hello world");
+		
+		try {
+			MainApplicationWindow window = new MainApplicationWindow();
+			window.setBlockOnOpen(true);
+			window.open();
+			Display.getCurrent().dispose();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
