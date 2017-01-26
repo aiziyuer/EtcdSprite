cf0d5224d646c59f4e10e218585cabf83aee44c5
susu <360858329@qq.com>
2017-01-27 00:03:09 +0800

    新增数据库查询工具类

--- src/main/java/com/aiziyuer/app/ssh/dao/SshInfoDAOImpl.java
+++ src/main/java/com/aiziyuer/app/ssh/dao/SshInfoDAOImpl.java
@@ -1,10 +1,19 @@
 package com.aiziyuer.app.ssh.dao;
 
 import com.aiziyuer.app.ssh.po.SshInfoPO;
+import lombok.Setter;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 
 @Log4j2
 public class SshInfoDAOImpl implements ISshInfoDAO {
+
+    @Setter
+	JdbcTemplate jdbcTemplate;
+
+    @Setter
+    NamedParameterJdbcTemplate namedTemplate;
 
 	@Override
 	public SshInfoPO queryAllSshInfoBos() {
