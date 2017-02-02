697dec6aefd0294a09dd338867c767d218cc926e
susu <360858329@qq.com>
2017-02-02 18:56:27 +0800

    第一次布局尝试(尚未成功)

--- src/main/java/com/aiziyuer/app/ui/MainApplicationWindow.java
+++ src/main/java/com/aiziyuer/app/ui/MainApplicationWindow.java
@@ -1,23 +1,42 @@
 package com.aiziyuer.app.ui;
 
+import java.util.List;
+
+import org.eclipse.e4.xwt.XWT;
+import org.eclipse.e4.xwt.annotation.UI;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Event;
 
 import com.aiziyuer.app.framework.util.ServiceLocator;
 import com.aiziyuer.app.ssh.biz.ISshInfoBiz;
+import com.aiziyuer.app.ssh.bo.SshInfoBO;
 
+import lombok.Getter;
 import lombok.extern.log4j.Log4j2;
 
 @Log4j2
 public class MainApplicationWindow {
 
+	@UI
+	private Composite rightContainer;
+
 	private ISshInfoBiz sshInfoBiz = ServiceLocator.getInstance()
 			.getBean("sshInfoBiz");
+	@Getter
+	private List<SshInfoBO> sshInfoBOs = sshInfoBiz.listSshInfoBOs();
 
-	public void clickBtn(Event event) {
+	public void onAboutMenuItemSelected(Event event) {
+		log.info("onAboutMenuItemSelect");
 
-		log.info(sshInfoBiz.listSshInfoBOs());
+		MessageDialog.openInformation(XWT.findShell(event.widget), "About",
+				"Thanks you for using. \nCopy right by aiziyuer.");
+	}
 
-		log.info("hello");
+	public void onSshTunnelMenuItemSelected(Event event) {
+		// 这里需要更新右边的视图
+
+		sshInfoBOs.addAll(sshInfoBiz.listSshInfoBOs());
 	}
 
 }
