8b0daa2e5166573033257f52fa0d0665e5695281
susu <360858329@qq.com>
2017-02-05 10:48:28 +0800

    实现一个可编辑的tableviewer

--- src/main/java/com/aiziyuer/app/ui/ssh/SshInfoTableCellModifier.java
+++ src/main/java/com/aiziyuer/app/ui/ssh/SshInfoTableCellModifier.java
@@ -1,12 +1,13 @@
 package com.aiziyuer.app.ui.ssh;
 
+import org.apache.commons.lang.ArrayUtils;
 import org.apache.commons.lang3.reflect.FieldUtils;
 import org.eclipse.jface.viewers.ICellModifier;
 import org.eclipse.jface.viewers.TableViewer;
+import org.eclipse.swt.widgets.TableItem;
 
 import lombok.Getter;
 import lombok.extern.log4j.Log4j2;
-
 
 /**
  *
@@ -14,41 +15,48 @@ import lombok.extern.log4j.Log4j2;
 @Log4j2
 public class SshInfoTableCellModifier implements ICellModifier {
 
-    @Getter
-    private TableViewer tv;
+	@Getter
+	private TableViewer tv;
 
-    public SshInfoTableCellModifier(TableViewer tv) {
-        super();
-        this.tv = tv;
-    }
+	public SshInfoTableCellModifier(TableViewer tv) {
+		super();
+		this.tv = tv;
+	}
 
-    @Override
-    public boolean canModify(Object element, String property) {
-        return true;
-    }
+	@Override
+	public boolean canModify(Object element, String property) {
+		return true;
+	}
 
-    @Override
-    public Object getValue(Object element, String property) {
+	@Override
+	public Object getValue(Object element, String property) {
 
-        Object ret = null;
+		Object ret = null;
 
-        try {
-            ret = FieldUtils.readDeclaredField(element, property, true);
-        } catch (IllegalAccessException e) {
-            log.error(e);
-        }
+		try {
+			ret = FieldUtils.readDeclaredField(element, property, true);
+		} catch (IllegalAccessException e) {
+			log.error(e);
+		}
 
-        return ret;
-    }
+		return ret;
+	}
 
-    @Override
-    public void modify(Object element, String property, Object value) {
+	@Override
+	public void modify(Object element, String property, Object value) {
 
-        try {
-            FieldUtils.writeDeclaredField(element, property, value, true);
-        } catch (IllegalAccessException e) {
-            log.error(e);
-        }
+		try {
+			TableItem item = (TableItem) element;
+			Object data = item.getData();
+			FieldUtils.writeDeclaredField(data, property, value, true);
 
-    }
+			int index = ArrayUtils.indexOf(tv.getColumnProperties(), property);
+			if (index != ArrayUtils.INDEX_NOT_FOUND) {
+				item.setText(index, String.valueOf(value));
+			}
+		} catch (IllegalAccessException e) {
+			log.error(e);
+		}
+
+	}
 }
