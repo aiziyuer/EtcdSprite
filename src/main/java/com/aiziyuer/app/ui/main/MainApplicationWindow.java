d4fd7ae56c912e35f131ba979db4f6d6281a76b8
susu <360858329@qq.com>
2017-02-06 20:42:36 +0800

    添加ViewerManger来进行Composite的管理,方便后续添加监听

--- src/main/java/com/aiziyuer/app/ui/main/MainApplicationWindow.java
+++ src/main/java/com/aiziyuer/app/ui/main/MainApplicationWindow.java
@@ -3,12 +3,11 @@ package com.aiziyuer.app.ui.main;
 import org.eclipse.e4.xwt.XWT;
 import org.eclipse.e4.xwt.annotation.UI;
 import org.eclipse.jface.dialogs.MessageDialog;
-import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Event;
 
-import com.aiziyuer.app.ui.ssh.SshInfoTableViewerComposite;
+import com.aiziyuer.app.ui.ssh.SshInfoTableViewerManager;
 
 import lombok.extern.log4j.Log4j2;
 
@@ -26,15 +25,16 @@ public class MainApplicationWindow {
 	}
 
 	public void onSshTunnelMenuItemSelected(Event event) {
-		
+
 		log.info("onSshTunnelMenuItemSelected");
 
 		// 释放右侧的区域内容
 		for (Control control : rightContainer.getChildren())
 			control.dispose();
 
-		new SshInfoTableViewerComposite(rightContainer, SWT.NONE);
-		
+		SshInfoTableViewerManager manager = new SshInfoTableViewerManager();
+		manager.buildUI(rightContainer);
+
 		// 触发重新布局以显示新的内容
 		rightContainer.layout();
 	}
--- src/main/java/com/aiziyuer/app/ui/ssh/SshInfoTableViewerComposite.java
+++ src/main/java/com/aiziyuer/app/ui/ssh/SshInfoTableViewerComposite.java
@@ -1,47 +1,12 @@
 package com.aiziyuer.app.ui.ssh;
 
-import java.net.URL;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import org.eclipse.e4.xwt.DefaultLoadingContext;
-import org.eclipse.e4.xwt.IConstants;
-import org.eclipse.e4.xwt.IXWTLoader;
-import org.eclipse.e4.xwt.XWT;
 import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.widgets.Composite;
 
-import com.aiziyuer.app.framework.util.ServiceLocator;
-import com.aiziyuer.app.ssh.biz.ISshInfoBiz;
-import com.aiziyuer.app.ssh.bo.SshInfoBO;
-
-import lombok.Getter;
-
 public class SshInfoTableViewerComposite extends Composite {
-
-	private ISshInfoBiz sshInfoBiz = ServiceLocator.getInstance()
-			.getBean("sshInfoBiz");
-	@Getter
-	private List<SshInfoBO> sshInfoBOs = sshInfoBiz.listSshInfoBOs();
 
 	public SshInfoTableViewerComposite(Composite parent, int style) {
 		super(parent, style);
 		setLayout(new FillLayout());
-		// load XWT
-		String name = SshInfoTableViewerComposite.class.getSimpleName()
-				+ IConstants.XWT_EXTENSION_SUFFIX;
-		try {
-			URL url = SshInfoTableViewerComposite.class.getResource(name);
-			Map<String, Object> options = new HashMap<String, Object>();
-			options.put(IXWTLoader.CLASS_PROPERTY, this);
-			options.put(IXWTLoader.CONTAINER_PROPERTY, this);
-			options.put(IXWTLoader.DATACONTEXT_PROPERTY, this);
-			XWT.setLoadingContext(new DefaultLoadingContext(
-					this.getClass().getClassLoader()));
-			XWT.loadWithOptions(url, options);
-		} catch (Throwable e) {
-			throw new Error("Unable to load " + name, e);
-		}
 	}
 }
