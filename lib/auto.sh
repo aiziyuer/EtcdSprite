1093a4fa9978f38447b0cc42a25ac39a57faf96f
susu <360858329@qq.com>
2017-02-08 23:56:32 +0800

    调整类库的名字

--- lib/auto.sh
+++ lib/auto.sh
@@ -1,15 +1,6 @@
 #!/usr/bin/env bash
 
-# mvn deploy:deploy-file \
-#     -DgroupId=com.aiziyuer \
-#     -DartifactId=com.aiziyuer.lombok \
-#     -Dversion=1.16.12 \
-#     -Dfile=com.aiziyuer.lombok-1.16.12.jar \
-#     -Dsources=com.aiziyuer.lombok-1.16.12-sources.jar \
-#     -Durl=file:../repo
-
-
-files=`ls | grep 'lipse.core.databinding.beans-1.2.100.I20100824-0800.jar'`
+files=` ls | grep jar | grep -v sources.jar `
 
 regex="(([a-z]+.[a-z]+).([^-]+)-(.*)).jar"
 
@@ -35,6 +26,6 @@ do
 		echo $command && eval $command
 
     else
-        echo "$f doesn't match" >&2 # this could get noisy if there are a lot of non-matching files
+        echo "$f doesn't match" >&2 
     fi
 done
